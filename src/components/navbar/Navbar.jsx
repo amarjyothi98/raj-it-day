422fc45d7c97cfafffe4c98aa4e06a704caf4734
Ram Goel <rgoel766@gmail.com>
2023-03-20 21:27:14 +0530

    navbar issues fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,22 +2,22 @@ import React,{useState} from 'react'
 
 export default function Navbar() {
 
-      const [style,setStyle]=useState(0)
+      const [style,setStyle]=useState(-200)
 
   return (
     <>
     <nav id="nav">
         <h2>FeedWell</h2>
         <div class="nav-links" id="navLinks" style={{right:`${style}px`}}>
-          <i class="fa fa-times" onclick={()=>setStyle(0)}></i>
+          <i class="fa fa-times text-dark" onClick={()=>setStyle(-200)}></i>
           <ul class="nav-mod">
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#ABOUT">ABOUT</a></li>
-            <li><a href="#course">CONTACT</a></li>
-            <li><a href="#blog">REVIEW</a></li>
+            <li style={{fontSize:'20px'}}><a href="#home">HOME</a></li>
+            <li style={{fontSize:'20px'}}><a href="#ABOUT">ABOUT</a></li>
+            <li style={{fontSize:'20px'}}><a href="#course">CONTACT</a></li>
+            <li style={{fontSize:'20px'}}><a href="#blog">REVIEW</a></li>
           </ul>
         </div>
-        <i class="fa fa-bars" onClick={()=>setStyle(-200)}></i>
+        <i class="fa fa-bars text-dark" onClick={()=>setStyle(0)}></i>
       </nav>
     </>
   )
