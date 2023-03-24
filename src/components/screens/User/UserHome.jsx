8662405869bed631fd8dce45130e127095bb64c7
Ram Goel <rgoel766@gmail.com>
2023-03-24 10:12:40 +0530

    imporved user home page

--- src/components/screens/User/UserHome.jsx
+++ src/components/screens/User/UserHome.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import image1 from '../../assets/addReview.png'
+import image2 from '../../assets/seeData.png'
+import { useNavigate } from 'react-router-dom'
 function UserHome() {
+    const navigate=useNavigate()
     return (
-        <div className='d-flex align-items-center justify-content-center' style={{height:'100vh'}}>
-            <div>
-            <Link to="/addreview">
-                <button className='btn btn-primary d-block my-1 w-100'><i className="fa fa-plus mx-2"></i>Add a Review</button>
-            </Link>
-            <Link to="/dashboard">
-                <button className='btn btn-primary d-block my-1 w-100'><i className="fa fa-eye mx-2"></i>See Reports</button>
-            </Link>
+        <div className='d-flex align-items-center justify-content-between w-100 bgimage' style={{ height: '100vh'}}>
+            <div className='row justify-content-between w-100'>
+
+                    <button onClick={()=>navigate('/addreview')} className='btn col-4 border bg-white rounded mx-auto '>
+                        <img src={image1} className="w-100" alt="" />
+                        Add a Review</button>
+
+                    <button onClick={()=>navigate('/dashboard')} className='btn col-4 border bg-white rounded mx-auto '>
+                        <img src={image2} className="w-100" alt="" />
+                        See Reports
+                    </button>
+            </div>
         </div>
-        </div>
-        
+
     )
 }
 
